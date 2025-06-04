7b46d4bfba615c63123da88bb1a2d54a8bf8a54c
ashag909 <goudaasharani86@gmail.com>
2025-06-04 23:55:27 +0530

    add sign-in and sign-up page

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
 from flask import Flask,render_template
+from flask_wtf import FlaskForm
+#it is used for form velidation and managing the form
+
+
 
 app = Flask(__name__)
+
+
+
+
+
+
 @app.route("/")
 def home():
     return render_template('index.html')
+@app.route("/signup")
+def signup():
+    render_template("signup.html")
+
+@app.route("/login")
+def login():
+    render_template("signin.html")
+
+@app.route("/membership")
+def member():
+    render_template("forms.html")
 
 if __name__=="__main__":
     app.run(debug=True)
