adad6005835bb1498f44dad51c3451c480d71ad2
ashag909 <goudaasharani86@gmail.com>
2025-06-05 23:56:40 +0530

    add wtfforms

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask,render_template
 from flask_wtf import FlaskForm
 #it is used for form velidation and managing the form
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import DataRequired,Email,ValidationError
 
 
 
 app = Flask(__name__)
 
-
-
+class Signupform(FlaskForm):
+    name=StringField("Name",validators=[DataRequired()])
+    email=StringField("Email",validators=[DataRequired(),Email()])
+    password=StringField("Password",validators=[DataRequired()])
+    submit=SubmitField("Register")
 
 
 
@@ -16,6 +21,7 @@ def home():
     return render_template('index.html')
 @app.route("/signup")
 def signup():
+    
     render_template("signup.html")
 
 @app.route("/login")
