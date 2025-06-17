da90c522ba2424dcc88f4c76849dbb7634e028a8
ashag909 <goudaasharani86@gmail.com>
2025-06-17 23:59:16 +0530

    add form ''

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_wtf import FlaskForm
 #it is used for form velidation and managing the form
 from wtforms import StringField,PasswordField,SubmitField
 from wtforms.validators import DataRequired,Email,ValidationError
-
+import bcrypt
 
 
 app = Flask(__name__)
@@ -21,6 +21,14 @@ def home():
     return render_template('index.html')
 @app.route("/signup")
 def signup():
+    form =Signupform()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        password=form.password.data
+
+
+        hashed_psw=bcrypt.hashpw(password.encode('utf-8'),bcrypt.gensalt())
     
     render_template("signup.html")
 
