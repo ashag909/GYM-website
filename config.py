f09bb3546a9bd0c8ef0e1821a31787a702ab0589
Asha Rani Gouda <goudaasharani86@gmail.com>
2025-09-15 14:51:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = "yuerbdsbnvyufv"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///gym.db"  # or MySQL/Postgres
+    SQLALCHEMY_DATABASE_URI = "sqlite:///gym.db" 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
