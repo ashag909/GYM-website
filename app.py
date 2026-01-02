cf7ecd24a61c63ae9e345490f1bee377bf473fc1
ashag909 <goudaasharani86@gmail.com>
2026-01-02 23:26:48 +0530

    added about page

--- app.py
+++ app.py
@@ -64,6 +64,8 @@ def contacts():
 def hom():
     return render_template("home.html")
 
-
+@app.route("/about")
+def about():
+    return render_template("about.html")
 if __name__ == "__main__":
     app.run(debug=True)
