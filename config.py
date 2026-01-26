8a51c0c77621614454ed187b44fafaee3400c468
ashag909 <goudaasharani86@gmail.com>
2026-01-26 23:44:08 +0530

    debug config

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = "yuerbdsbnvyufv"
+    SECRET_KEY = os.environ.get('SECRET_KEY','dev-secret')
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Ilovemyfamily%40143@localhost/gymdb' 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
